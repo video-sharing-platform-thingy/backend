a0b4163fb927265a38370951089824766168339b
Kognise <felix.mattick@gmail.com>
2019-03-10 20:30:12 -0400

    Remove check function and comment everything + delete uploaded files

--- utils/transcode.go
+++ utils/transcode.go
@@ -9,19 +9,27 @@ import (
 
 // Transcode transcodes a video file.
 func Transcode(fromPath string, toPath string) error {
+	// Get the current time
 	start := time.Now()
+
+	// Initialize a new transcoder
 	trans := new(transcoder.Transcoder)
 	err := trans.Initialize(fromPath, toPath)
 	if err != nil {
 		return err
 	}
+
+	// Run the transcoding
 	done := trans.Run(false)
 	err = <-done
 	if err != nil {
 		return err
 	}
+
+	// Get the elapsed time and print it
 	t := time.Now()
 	elapsed := t.Sub(start)
 	log.Println("Transcoded in", elapsed)
+
 	return nil
 }
